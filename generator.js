29cd17eca5005a0a8ca49dd86da3820869129a6d
Phúc Vô <vop844940@gmail.com>
2025-09-26 11:51:59 +0000

    fix: correct HTML entity for less than sign in generator.js
    Co-authored-by: Genie <genie@cosine.sh>

--- generator.js
+++ generator.js
@@ -1,7 +1,7 @@
 function generateCode(length = 12) {
   const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
   let code = "";
-  for (let i = 0; i &lt; length; i++) {
+  for (let i = 0; i < length; i++) {
     code += chars.charAt(Math.floor(Math.random() * chars.length));
   }
   return code;
@@ -9,7 +9,7 @@ function generateCode(length = 12) {
 
 function generateMultipleCodes(count = 10, length = 12) {
   const codes = [];
-  for (let i = 0; i &lt; count; i++) {
+  for (let i = 0; i < count; i++) {
     codes.push(generateCode(length));
   }
   return codes;
