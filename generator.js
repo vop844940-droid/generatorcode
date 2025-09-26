2aec40351363b8686420142a6ac29fc9457bfba5
Phúc Vô <vop844940@gmail.com>
2025-09-26 11:51:06 +0000

    feat: add code generator functions to generate random codes
    Co-authored-by: Genie <genie@cosine.sh>

---
+++ generator.js
@@ -0,0 +1,19 @@
+function generateCode(length = 12) {
+  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let code = "";
+  for (let i = 0; i &lt; length; i++) {
+    code += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return code;
+}
+
+function generateMultipleCodes(count = 10, length = 12) {
+  const codes = [];
+  for (let i = 0; i &lt; count; i++) {
+    codes.push(generateCode(length));
+  }
+  return codes;
+}
+
+// Exemplu: generează 10 coduri de 12 caractere
+console.log(generateMultipleCodes(10, 12));
